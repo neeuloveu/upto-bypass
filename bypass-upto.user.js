49b622456b53cc59f5f8ef87df960ca0be4dbc4c
neeuloveu <neeiyeuem@gmail.com>
2026-05-17 11:33:59 +0700

    Create bypass-upto.user.js

---
+++ bypass-upto.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Uptolink
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bypass
+// @require      https://raw.githubusercontent.com/neeuloveu/upto-bypass/refs/heads/main/neei-scr.js
+// @match        *://*/*
+// @author       neeiloveu
+// @grant        GM_xmlhttpRequest
+// @connect      uptolink.one
+// @connect      raw.githubusercontent.com
+// @connect      api.github.com
+// @connect      generativelanguage.googleapis.com
+// @connect      api.kolosal.ai
+// @connect      *
+// @cun-at       document-end
+// ==/UserScript==
